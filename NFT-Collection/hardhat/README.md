cb4bc68ae1a71dbf1dc3daad31279c4032fa7286
CJskii <czareczarek3@gmail.com>
2022-10-12 02:16:22 +0100

    Added metadata

--- NFT-Collection/hardhat/README.md
+++ NFT-Collection/hardhat/README.md
@@ -11,4 +11,5 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
 ```
-contract : 0xA0848EBb7f37efb132a960DAE5F9E079dacBd1ed
+contract1 : 0xA0848EBb7f37efb132a960DAE5F9E079dacBd1ed
+contract2 : 0xA3586Ba3798907bD938d0a762eFa86E42B3C54b9
--- NFT-Collection/hardhat/constants/index.js
+++ NFT-Collection/hardhat/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x932424cEE2AEff1f52166175fEC6E6387f76b21a";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-five-nu.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
