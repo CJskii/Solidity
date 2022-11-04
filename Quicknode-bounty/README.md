d0fd9f8b813233f13acb04393a7560b5ebc48559
CJskii <czareczarek3@gmail.com>
2022-11-04 02:08:49 +0000

    bounty submission

---
+++ Quicknode-bounty/README.md
@@ -0,0 +1,16 @@
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
+
+Mumbai contract address: 0x1dF72404F60AeA1c93E628D839095b88972B0487
+Transfer: https://mumbai.polygonscan.com/token/0x1df72404f60aea1c93e628d839095b88972b0487
---
+++ Quicknode-bounty/contracts/NFT.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Quicknode is ERC721Enumerable, Ownable {
+    
+    uint256 public cost = 0.01 ether;
+    uint256 public maxSupply = 100;
+    uint256 public maxMintAmount = 10;
+
+    constructor(
+  ) ERC721("Quick", "NODE") {
+        _mint(msg.sender, 2);
+    }
+
+   function mint(uint256 _mintAmount) public payable {
+    uint256 supply = totalSupply();
+    require(_mintAmount > 0);
+    require(_mintAmount <= maxMintAmount);
+    require(supply + _mintAmount <= maxSupply);
+
+    if (msg.sender != owner()) {
+      require(msg.value >= cost * _mintAmount);
+    }
+
+    for (uint256 i = 1; i <= _mintAmount; i++) {
+      _safeMint(msg.sender, supply + i);
+    }
+  }
+
+    function walletOfOwner(address _owner)
+    public
+    view
+    returns (uint256[] memory)
+  {
+    uint256 ownerTokenCount = balanceOf(_owner);
+    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+    for (uint256 i; i < ownerTokenCount; i++) {
+      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    }
+    return tokenIds;
+  }
+
+    function setCost(uint256 _newCost) public onlyOwner {
+        cost = _newCost;
+    }
+
+    function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner {
+        maxMintAmount = _newmaxMintAmount;
+    }
+}
+
